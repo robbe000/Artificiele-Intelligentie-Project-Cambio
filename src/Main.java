23859c03574f0ee297a76ff2268c0378ac3f20e6
robbe000 <robbe_goovaerts@telenet.be>
2019-02-18 10:04:15 +0100

    main.java aangemaakt

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+import gegevens.*;
+import oplossing.*;
+
+public class Main {
+	ArrayList<Reservatie> reservaties;
+	ArrayList<Voertuig> voertuigen;
+	ArrayList<Zone> zones;
+	ArrayList<AReservatie> areservaties;
+	ArrayList<AVoertuig> avoertuigen;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
