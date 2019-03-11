9ae5ec738eaca0e871f572ee5e7a33206a8a611b
E-pep <paul.leroy1996@hotmail.com>
2019-03-11 11:53:47 +0100

    random in local search toegevoegd

--- src/LokaalZoeken/Zoeken.java
+++ src/LokaalZoeken/Zoeken.java
@@ -1,5 +1,7 @@
 package LokaalZoeken;
 
+import java.util.Random;
+
 import gegevens.*;
 import oplossing.*;
 
@@ -7,8 +9,19 @@ public class Zoeken {
 	private AOplossing besteOplossing;
 	
 	public AOplossing zoeken(AOplossing oplossing) {
-		
 		return oplossing;
+	}
+	
+	private AVoertuig random(AOplossing oplossing)
+	{
+		Random rand = new Random();
+		AVoertuig voertuig = oplossing.getVoertuig().get(rand.nextInt(oplossing.getVoertuig().size()));
+		
+		Random rand2 = new Random();
+		oplossing.getReservatie().get(rand2.nextInt(oplossing.getReservatie().size())).setVoertuig(voertuig);
+		oplossing.getReservatie().get(rand2.nextInt(oplossing.getReservatie().size())).setVoertuigId(voertuig.getVoertuigId());
+		
+		return voertuig;
 	}
 
 }
