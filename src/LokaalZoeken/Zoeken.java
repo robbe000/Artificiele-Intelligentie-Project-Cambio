682f60e1a40010b6b72ee9907e0fe379ef938871
robbe000 <robbe_goovaerts@telenet.be>
2019-03-11 10:31:54 +0100

    Start lokaal zoeken

---
+++ src/LokaalZoeken/Zoeken.java
@@ -0,0 +1,14 @@
+package LokaalZoeken;
+
+import gegevens.*;
+import oplossing.*;
+
+public class Zoeken {
+	
+	public AOplossing zoeken(AOplossing oplossing) {
+		
+		
+		return oplossing;
+	}
+
+}
---
+++ src/oplossing/AOplossing.java
@@ -0,0 +1,22 @@
+package oplossing;
+import gegevens.*;
+import oplossing.*;
+
+public class AOplossing {
+	private AReservatie reservatie;
+	private AVoertuig voertuig;
+	public AReservatie getReservatie() {
+		return reservatie;
+	}
+	public void setReservatie(AReservatie reservatie) {
+		this.reservatie = reservatie;
+	}
+	public AVoertuig getVoertuig() {
+		return voertuig;
+	}
+	public void setVoertuig(AVoertuig voertuig) {
+		this.voertuig = voertuig;
+	}
+	
+	
+}
