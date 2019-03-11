2211fd41e3efaf79e54105a1c7d62cc990e2740d
robbe000 <robbe_goovaerts@telenet.be>
2019-03-11 10:58:07 +0100

    LinkToRes functie gemaakt

--- src/LokaalZoeken/Zoeken.java
+++ src/LokaalZoeken/Zoeken.java
@@ -10,5 +10,21 @@ public class Zoeken {
 		
 		return oplossing;
 	}
+	
+	//Linken aan nieuwe mogelijke reservatie
+	private AOplossing linkToRes(AVoertuig voertuig, AOplossing oplossing) {
+		for(AReservatie reservatie : oplossing.getReservatie()) {
+			//In de gewenste zone? => Dit hebben we het liefst!
+			if(reservatie.getGewZoneId() == voertuig.getZoneId()) {
+				reservatie.setVoertuig(voertuig);
+				reservatie.setVoertuigId(voertuig.getVoertuigId());
+				return oplossing;
+			}
+			//Ook nog met omliggende zones!
+			
+		}		
+		
+		return oplossing;
+	}
 
 }
